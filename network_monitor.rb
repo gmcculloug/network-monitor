c00792bbaaa2230b81bc06c68461774a20c5d48b
Greg McCullough <gmccullo@redhat.com>
2021-03-10 15:11:31 -0500

    Write output to file by date

--- network_monitor.rb
+++ network_monitor.rb
@@ -1,10 +1,11 @@
+require 'time'
+require 'date'
+
 PING_COUNT = 60
 PACKET_LOSS_ACCEPTABLE_LIMIT = 10
-SUCCESSFUL_PING_DELAY_IN_SECS = 300 - PING_COUNT
+SUCCESSFUL_PING_DELAY_IN_SECS = 300 - PING_COUNT   # 5 mins - Ping time
 PING_TARGET_ADDRESS = '8.8.8.8'.freeze
 PING_STATS_REGEX = /(?<transmitted>\d+) .* (?<received>\d+) .* (?<loss_pct>\d+.?\d?+)%/.freeze
-
-require 'time'
 
 def run_ping
   all_output = `ping -c #{PING_COUNT} #{PING_TARGET_ADDRESS}`.split("\n")
@@ -21,6 +22,11 @@ def parse_stats(packet_stats)
   }
 end
 
+def write_output(data)
+  puts data
+  File.open("#{Date.today.iso8601}.txt", 'a') { |f| f.write data + "\n" }
+end
+
 seq = 1
 highest_loss_pct = 0
 program_start_time = Time.now
@@ -33,24 +39,14 @@ begin
     elapsed_time = end_time - start_time
     highest_loss_pct = stats[:loss_pct] if highest_loss_pct < stats[:loss_pct]
 
-    puts "SEQ:[#{seq}] #{stats.inspect} finished at [#{end_time.iso8601}] after #{elapsed_time.to_i} seconds"
-
-    if stats[:loss_pct] >= PACKET_LOSS_ACCEPTABLE_LIMIT
-      puts
-      puts all_output.join("\n")
-      puts
-      puts stats.inspect
-    end
+    write_output "SEQ:[#{seq}] #{stats.inspect} finished at [#{end_time.iso8601}] after #{elapsed_time.to_i} seconds"
+    write_output "#{all_output.join("\n")}\n#{stats.inspect}\n" if stats[:loss_pct] >= PACKET_LOSS_ACCEPTABLE_LIMIT
 
     sleep(SUCCESSFUL_PING_DELAY_IN_SECS) if stats[:loss_pct].zero?
     seq += 1
   end
 rescue Interrupt
-  puts
-  puts "Highest loss percentage: [#{highest_loss_pct}]"
-  puts "Run times:"
-  puts "Start:   #{program_start_time.iso8601}"
-  puts "Stop:    #{Time.now.iso8601}"
-  puts "Elapsed: #{Time.now - program_start_time}"
+  write_output "Highest loss percentage: [#{highest_loss_pct}]"
+  write_output "Run times - Start: #{program_start_time.iso8601}  -  Stop: #{Time.now.iso8601}  -  Elapsed: #{Time.now - program_start_time}"
   exit
 end
